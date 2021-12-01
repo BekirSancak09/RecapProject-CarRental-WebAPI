5f4e59ff6fa1748ccf96ca097a6675377144e725
Bekir Sancak <37635378+BekirSancak09@users.noreply.github.com>
2021-12-02 00:58:50 +0300

    Add files via upload

---
+++ WebAPI/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using Business.DependencyResolvers.Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+
+namespace WebAPI
+{
+    /// <summary>
+    /// Api'ye senin kendi IOC 'i yapýn var ama Ben Autofac'in IOC 'i yapýsýný kullanmak istiyorum diyoruz.
+    /// .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    /// .ConfigureContainer<ContainerBuilder>(builder=>
+    ///  {
+    ///        builder.RegisterModule(new AutofacBusinessModule());
+    ///  }) 
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                  .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                  .ConfigureContainer<ContainerBuilder>(builder => {
+
+                      builder.RegisterModule(new AutofacBusinessModule());
+                  })
+                  
+                   .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
